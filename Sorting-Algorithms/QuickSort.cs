691ef3ae5bc872b30cfef65be2fe1878dd5a668c
Emmanuel Opoku <Emmanuel.Opoku@serko.com>
2020-05-25 14:21:45 -0500

    added quicksort method to sort int array

---
+++ Sorting-Algorithms/QuickSort.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructure
+{
+    public class SortingAlgorithm
+    {
+        /* The main function that implements QuickSort() 
+            arr[] --> Array to be sorted, 
+            low --> Starting index, 
+        high --> Ending index */
+        public void QuickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                // pi i partitioning index, arr[pi] is now at right place */
+                int pi = Partition(arr, low, high);
+
+                // Recursively sort elements before Partition and after partition
+                QuickSort(arr, low, pi - 1);
+                QuickSort(arr, pi + 1, high);
+            }
+        }
+
+        private int Partition(int[] arr, int low, int high)
+        {
+            /* This function takes last element as pivot, 
+                places the pivot element at its correct 
+                position in sorted array, and places all 
+                smaller (smaller than pivot) to left of 
+                pivot and all greater elements to right 
+            of pivot */
+
+            int pivot = arr[high];
+
+            // index of smaller element
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                // if current element is smaller than pivot
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    // perform swap operation
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            // swap arr[i+1] and arr[high] (or pivot)
+            int temp1 = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp1;
+
+            return i + 1;
+
+        }
+
+        public void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
