a504c7fbc14f3f3dffa0ee6a59083ebdd4954452
Emmanuel Opoku <Emmanuel.Opoku@serko.com>
2020-05-25 23:34:03 -0500

    added method yo get the Nth index

--- LinkedList/SearchNodeInLinkedList.cs
+++ LinkedList/SearchNodeInLinkedList.cs
@@ -28,3 +28,23 @@
 
             return RecursiveSearch(head.next, x);
         }
+
+        /* Takes head pointer of the linked list and index  
+        as arguments and return data at index*/
+        static int GetNth( Node head, int n)  
+        {  
+            //Base Condition 
+            if(head == null) 
+                return -1; 
+  
+            int count = 0;  
+      
+            //if count equal too n return node.data  
+            //Test Condition 
+            if(count == n)  
+                return head.data;  
+      
+            //recursively decrease n and increase  
+            // head to next pointer  
+             return GetNth(head.next, n - 1);  
+        }  
