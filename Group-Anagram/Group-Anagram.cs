3d821bb6193d7bffa698264fb948cdee7678b0d6
Emmanuel Opoku <Emmanuel.Opoku@serko.com>
2020-05-28 23:08:37 -0500

    updated method

--- Group-Anagram/Group-Anagram.cs
+++ Group-Anagram/Group-Anagram.cs
@@ -7,6 +7,12 @@ namespace DataStructure
 {
     public class SortedZeros
     {
+        /*
+            Time Complexity: O(NK \log K)O(NKlogK), where NN is the length of strs, and KK is the maximum length of a string in strs. 
+            The outer loop has complexity O(N)O(N) as we iterate through each string. Then, we sort each string in O(K \log K)O(KlogK) time.
+            Space Complexity: O(NK)O(NK), the total information content stored in ans.
+
+         */
         public IList<IList<string>> GroupAnagrams(string[] strs)
         {
             var anagrams = new List<IList<string>>();
