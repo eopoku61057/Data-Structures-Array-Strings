d6546d8cc3e90b790ff2481868fc38eee125c3e5
Emmanuel Opoku <Emmanuel.Opoku@serko.com>
2020-06-13 22:49:50 -0500

    added alien dictionary

--- TREES-AND-GRAPHS-LEETCODE/Alien-Dictionary.cs
+++ TREES-AND-GRAPHS-LEETCODE/Alien-Dictionary.cs
@@ -6,82 +6,87 @@ using System.Text;
 
 namespace DataStructure
 {
-    public class SortedZeros
+    public class NumMatrix
     {
-        // Time complexity: 0(C)
-        // space complexity: 0(1)
-        private Dictionary<char, List<char>> reverseAdjList = new Dictionary<char, List<char>>();
-        private Dictionary<char, Boolean> seen = new Dictionary<char, bool>();
-        private StringBuilder output = new StringBuilder();
-        public string AlienOrder(string[] words)
+        public string alienOrder(string[] words)
         {
-            //step 0: put all unique letters into reverseAdjList as keys
-            foreach (var word in words)
+            int[] indegree = new int[26];
+            Dictionary<char, List<char>> g = new Dictionary<char, List<char>>();
+            BuildGraph(g, words, indegree);
+
+            return BFS(g, indegree);
+        }
+
+        private string BFS(Dictionary<char, List<char>> g, int[] indegree)
+        {
+            StringBuilder sb = new StringBuilder();
+            int totalChars = g.Count();
+            Queue<char> queue = new Queue<char>();
+
+            foreach (char c in g.Keys)
             {
-                foreach(var c in word.ToCharArray())
+                if (indegree[c - 'a'] == 0)
                 {
-                    reverseAdjList.Add(c, new List<char>());
+                    sb.Append(c);
+                    queue.Enqueue(c);
                 }
             }
 
-            // step1: Find all edges and add reverse edges to reverseAdjList.
-            for (int i = 0; i < words.Length - 1; i++)
+            while (queue.Count > 0)
             {
-                string word1 = words[i];
-                string word2 = words[i + 1];
-                // check that word2 is not prefix of word1
-                if (word1.Length > word2.Length && word1.StartsWith(word2))
+                char curr = queue.Peek();
+                if (g.GetValueOrDefault(curr) != null || g.GetValueOrDefault(curr).Count == 0) continue;
+
+                foreach (char neighbor in g.GetValueOrDefault(curr))
                 {
-                    return "";
-                }
-                // Find the first non match and insert the corresponding relation
-                for (int j = 0; j < Math.Min(word1.Length, word2.Length); j++)
-                {
-                    if(word1[j] != word2[j])
+                    indegree[neighbor - 'a']--;
+                    if (indegree[neighbor - 'a'] == 0)
                     {
-                        reverseAdjList.GetValueOrDefault(word2[j]).Add(word1[j]);
+                        queue.Enqueue(neighbor);
+                        sb.Append(neighbor);
+                    }
+                }
+            }
+
+            return sb.Length == totalChars ? sb.ToString() : "";
+        }
+
+        private void BuildGraph(Dictionary<char, List<char>> g, string[] words, int[] indegree)
+        {
+            foreach (var word in words)
+            {
+                foreach (var c in word)
+                {
+                    if (!g.ContainsKey(c))
+                    {
+                        g.Add(c, new List<char>());
+                    }
+                    
+                }
+            }
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                string first = words[i - 1];
+                string second = words[i];
+                int len = Math.Min(first.Length, second.Length);
+
+                for (int j = 0; j < len; j++)
+                {
+                    if(first[j] != second[j])
+                    {
+                        char out1 = first[j];
+                        char in1 = second[j];
+
+                        if (!g.GetValueOrDefault(out1).Contains(in1))
+                        {
+                            g.GetValueOrDefault(out1).Add(in1);
+                            indegree[in1 - 'a']++;
+                        }
                         break;
                     }
                 }
             }
-
-            // step 2: DFS to build up the output list
-            foreach (var i in words)
-            {
-                foreach (var x in i)
-                {
-                    if (reverseAdjList.ContainsKey(i[x]))
-                    {
-                        bool result = dfs(x);
-                        if (!result) return "";
-                    }
-                }
-            }
-
-            if (output.Length < reverseAdjList.Count())
-            {
-                return "";
-            }
-            return output.ToString();
-
-        }
-
-        // return true if no cycles detected
-        private bool dfs(char x)
-        {
-            if (seen.ContainsKey(x))
-            {
-                return seen.GetValueOrDefault(x); // if this node was grey (false), a cycle was detected
-            }
-            seen.Add(x, false);
-            foreach (var next in reverseAdjList.GetValueOrDefault(x))
-            {
-                bool result = dfs(next);
-                if (!result) return false;
-            }
-            seen.Add(x, false);
-            output.Append(x);
-            return true;
         }
     }
 }
