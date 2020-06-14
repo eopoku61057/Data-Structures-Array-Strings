e3d906f9d169ce5c1b8d1877bbd34458f3e8a75b
Emmanuel Opoku <Emmanuel.Opoku@serko.com>
2020-06-14 11:12:38 -0500

    added method for account merge

--- TREES-AND-GRAPHS-LEETCODE/Account-Merge/account-merge.cs
+++ TREES-AND-GRAPHS-LEETCODE/Account-Merge/account-merge.cs
@@ -1,40 +1,49 @@
-/*
-Given a list accounts, each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, 
-and the rest of the elements are emails representing emails of the account.
-
-Now, we would like to merge these accounts. Two accounts definitely belong to 
-the same person if there is some email that is common to both accounts. Note that even if two accounts have the same name, t
-hey may belong to different people as people could have the same name. A person can have any number of accounts initially, 
-but all of their accounts definitely have the same name.
-
-After merging the accounts, return the accounts in the following format: the first element of each account is the name, and 
-the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
-
-Time Complexity: O(A \log A)
-Space Complexity: O(A), the space used by our DSU structure.
- */
-
-// NOTE: In progress, not fully done
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace DataStructure
 {
-    public class SortedZeros
+    public class NumMatrix
     {
+        public class DSU
+        {
+            int[] parent;
+            public DSU()
+            {
+                parent = new int[10001];
+                for (int i = 0; i < 10001; i++)
+                {
+                    parent[i] = i;
+                }
+            }
+
+            public int find(int x)
+            {
+                if (parent[x] != x)
+                {
+                    parent[x] = find(parent[x]);
+                }
+                return parent[x];
+            }
+
+            public void union(int x, int y)
+            {
+                parent[find(x)] = find(y);
+            }
+        }
         public IList<IList<string>> AccountsMerge(IList<IList<string>> accounts)
         {
             DSU dsu = new DSU();
-            Dictionary<string, string> emailToName = new Dictionary<string, string>();
-            Dictionary<string, int> emailToID = new Dictionary<string, int>();
+            var emailToName = new Dictionary<string, string>();
+            var emailToID = new Dictionary<string, int>();
             int id = 0;
-           
-            foreach(var account in accounts)
+            foreach(IList<string> account in accounts)
             {
                 string name = "";
-                foreach(string email in account)
+                foreach (string email in account)
                 {
                     if(name == "")
                     {
@@ -46,42 +55,38 @@ namespace DataStructure
                     {
                         emailToID.Add(email, id++);
                     }
-                    dsu.union(emailToID[email], emailToID[email]);
+                    dsu.union(emailToID.GetValueOrDefault(account[1]), emailToID.GetValueOrDefault(email));
                 }
             }
 
-            Dictionary<int, List<string>> ans = new Dictionary<int, List<string>>();
-            foreach (var email in emailToName.Keys)
+            var ans = new Dictionary<int, List<string>>();
+            foreach ( string email in emailToName.Keys)
             {
-                int index = dsu.find(emailToID[email]);
-                ans.Where(x => x.)
+                int index = dsu.find(emailToID.GetValueOrDefault(email));
+                if (!ans.ContainsKey(index))
+                {
+                    var dd = new List<string>();
+                    dd.Add(email);
+                    ans.Add(index, dd);
+                }
             }
-            foreach  (List<string> component in ans.Values)
-            {
-                List.Enumerator(component);
-                component.add(0, emailToName.get(component.get(0)));
-            }
-            return new ArrayList(ans.values());
 
-        }
-        public class DSU
-        {
-            public int[] parent;
-            public DSU()
+            foreach (IList<string> component in ans.Values)
             {
-                parent = new int[10001];
-                for (int i = 0; i <= 10000; ++i)
-                    parent[i] = i;
+                _ = component.OrderBy(x => x);
+                component.Add(emailToName.GetValueOrDefault(component[0]));
             }
-            public int find(int x)
+
+            // getting the return data
+            IList<IList<string>> output = new List<IList<string>>();
+            foreach (var d in ans.Values)
             {
-                if (parent[x] != x) parent[x] = find(parent[x]);
-                return parent[x];
+                if (!output.Contains(d))
+                {
+                    output.Add(d);
+                }
             }
-            public void union(int x, int y)
-            {
-                parent[find(x)] = find(y);
-            }
+            return output;
         }
     }
 }
