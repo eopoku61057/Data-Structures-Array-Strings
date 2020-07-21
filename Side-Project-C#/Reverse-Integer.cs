cf7ac3b3e7ae3546e2aa32434dc2411250f2b6e4
Emmanuel Opoku <Emmanuel.Opoku@serko.com>
2020-07-20 20:53:20 -0500

    updated Reverse interger

--- Side-Project-C#/Reverse-Integer.cs
+++ Side-Project-C#/Reverse-Integer.cs
@@ -17,7 +17,7 @@ Output: 321
             x /= 10;
 
             if (rev > Int.MaxValue / 10 || (rev == Int.MaxValue / 10 && pop > 7)) return 0;
-            if (rev < Int.MaxValue / 10 || (rev == Int.MaxValue / 10 && pop < -8)) return 0;
+            if (rev < Int.MinValue / 10 || (rev == Int.MinValue / 10 && pop < -8)) return 0;
             rev = rev * 10 + pop;
         }
         return rev;
